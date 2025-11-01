0a895bf8cf3909e892f02676c7ba6170db9cde6a
isard <isard@debian>
2025-11-01 18:03:57 +0100

    tests.py definitiu funcionant tots els casos

--- polls/tests.py
+++ polls/tests.py
@@ -2,8 +2,10 @@ from django.test import TestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from django.contrib.auth.models import User
+import time
 
 class MySeleniumTests(StaticLiveServerTestCase):
     # carregar una BD de test
@@ -68,39 +70,125 @@ class MySeleniumTests(StaticLiveServerTestCase):
 
         # utilitzem assertNotEqual per testejar que NO hem entrat
         self.assertNotEqual( self.selenium.title , "Site administration | Django site admin" )
+        time.sleep(3)
 
-    # def test_login_crear_usuari(self):
-    #     # anem directament a la pàgina d'accés a l'admin panel
-    #     self.selenium.get('%s%s' % (self.live_server_url, '/admin/login/'))
+    def test_login_crear_usuari(self):
+        # anem directament a la pàgina d'accés a l'admin panel
+        self.selenium.get('%s%s' % (self.live_server_url, '/admin/login/'))
 
-    #     # comprovem que el títol de la pàgina és el que esperem
-    #     self.assertEqual( self.selenium.title , "Log in | Django site admin" )
+        # comprovem que el títol de la pàgina és el que esperem
+        self.assertEqual( self.selenium.title , "Log in | Django site admin" )
 
-    #     # introduïm dades de login i cliquem el botó "Log in" per entrar
-    #     username_input = self.selenium.find_element(By.NAME,"username")
-    #     username_input.send_keys('isard')
-    #     password_input = self.selenium.find_element(By.NAME,"password")
-    #     password_input.send_keys('pirineus')
-    #     self.selenium.find_element(By.XPATH,'//input[@value="Log in"]').click()
-    #     # accedim a users
-    #     self.selenium.find_element(By.LINK_TEXT, "Users").click()
+        # introduïm dades de login i cliquem el botó "Log in" per entrar
+        username_input = self.selenium.find_element(By.NAME,"username")
+        username_input.send_keys('isard')
+        password_input = self.selenium.find_element(By.NAME,"password")
+        password_input.send_keys('pirineus')
+        self.selenium.find_element(By.XPATH,'//input[@value="Log in"]').click()
+        # accedim a users
+        self.selenium.find_element(By.LINK_TEXT, "Users").click()
 
-    #     # afegim usuari
-    #     self.selenium.find_element(By.LINK_TEXT, "ADD USER").click()
+        # afegim usuari
+        self.selenium.find_element(By.LINK_TEXT, "ADD USER").click()
 
-    #     # omplim formulari
-    #     self.selenium.find_element(By.NAME, "username").send_keys("usuari_staf")
-    #     self.selenium.find_element(By.NAME, "password1").send_keys("usrDjango25$")
-    #     self.selenium.find_element(By.NAME, "password2").send_keys("usrDjango25$")
+        # omplim formulari
+        self.selenium.find_element(By.NAME, "username").send_keys("usuari_staff")
+        self.selenium.find_element(By.NAME, "password1").send_keys("usrDjango25$")
+        self.selenium.find_element(By.NAME, "password2").send_keys("usrDjango25$")
 
-    #     # fer clik al botó save and continue editing
-    #     self.selenium.find_element(By.NAME, "_continue").click()
+        # fer clik al botó save and continue editing
+        self.selenium.find_element(By.NAME, "_continue").click()
 
-    #     # a la següent plana
-    #     # seleccionem el permís "Can view user" al selector múltiple de permisos
-    #     from selenium.webdriver.support.ui import Select
-    #     permisos_select = Select(self.selenium.find_element(By.ID, "id_user_permissions"))
-    #     permisos_select.select_by_visible_text("Authentication and Authorization | user | Can view user")
+        # a la següent plana
+        self.selenium.find_element(By.NAME, "is_staff").click()
+        select_element = self.selenium.find_element(By.ID, "id_user_permissions_from")
+        time.sleep(10)
+        select = Select(select_element)
+        select.select_by_visible_text("Authentication and Authorization | user | Can view user")
+        # select.select_by_value("24")
+        # Al principi vaig optar per fer la selecció pel valor però més tard em vaig adonar que 
+        # si es carrega el formulari varies vegades, asigna valors diferents així que vaig decidir
+        # buscar pel text.
 
-    #     # guardem l'usuari definitiu
-    #     self.selenium.find_element(By.NAME, "_save").click()
+        self.selenium.find_element(By.ID, "id_user_permissions_add").click()
+        # faig temps per veure que ha seleccionat el permís correcte
+        time.sleep(3)
+
+        # guardem l'usuari definitivament
+        self.selenium.find_element(By.NAME, "_save").click()
+
+        # afegim usuari1
+        self.selenium.find_element(By.LINK_TEXT, "ADD USER").click()
+
+        # omplim formulari
+        self.selenium.find_element(By.NAME, "username").send_keys("usuari1")
+        self.selenium.find_element(By.NAME, "password1").send_keys("usrDjango25$")
+        self.selenium.find_element(By.NAME, "password2").send_keys("usrDjango25$")
+        # guardem l'usuari una vegada per cadasquna de les dues planes
+        self.selenium.find_element(By.NAME, "_save").click()
+        self.selenium.find_element(By.NAME, "_save").click()
+
+
+        # afegim usuari
+        self.selenium.find_element(By.LINK_TEXT, "ADD USER").click()
+
+        # omplim formulari
+        self.selenium.find_element(By.NAME, "username").send_keys("usuari2")
+        self.selenium.find_element(By.NAME, "password1").send_keys("usrDjango25$")
+        self.selenium.find_element(By.NAME, "password2").send_keys("usrDjango25$")
+        # guardem l'usuari una vegada per cadasquna de les dues planes
+        self.selenium.find_element(By.NAME, "_save").click()
+        self.selenium.find_element(By.NAME, "_save").click()
+
+
+        # afegim usuari
+        self.selenium.find_element(By.LINK_TEXT, "ADD USER").click()
+
+        # omplim formulari
+        self.selenium.find_element(By.NAME, "username").send_keys("usuari3")
+        self.selenium.find_element(By.NAME, "password1").send_keys("usrDjango25$")
+        self.selenium.find_element(By.NAME, "password2").send_keys("usrDjango25$")
+        # guardem l'usuari una vegada per cadasquna de les dues planes
+        self.selenium.find_element(By.NAME, "_save").click()
+        self.selenium.find_element(By.NAME, "_save").click()
+
+        # faig temps per veure que tots el usuris s'han creat
+        time.sleep(1)
+
+        # sortirm del formulari ja que si iniciem un altre test no hi són els usuaris
+        self.selenium.find_element(By.ID, "logout-form").click()
+
+        time.sleep(1)
+
+        # busquem iniciar de nou la sesió
+        self.selenium.find_element(By.LINK_TEXT,"Log in again").click()
+        time.sleep(1)
+
+        # comprovem que el títol de la pàgina és el que esperem
+        self.assertEqual( self.selenium.title , "Log in | Django site admin" )
+
+        # faig el focus al username
+        self.selenium.find_element(By.NAME, "username").click()
+
+        # introduïm dades de login i cliquem el botó "Log in" per entrar
+        username_input = self.selenium.find_element(By.NAME,"username")
+        username_input.send_keys('usuari_staff')
+        password_input = self.selenium.find_element(By.NAME,"password")
+        password_input.send_keys('usrDjango25$')
+        self.selenium.find_element(By.XPATH,'//input[@value="Log in"]').click()
+
+        time.sleep(1)
+
+        # accedim a users
+        self.selenium.find_element(By.LINK_TEXT, "Users").click()
+
+        time.sleep(1)
+
+        # seleccionem usuari1
+        self.selenium.find_element(By.LINK_TEXT, "usuari1").click()
+
+        # Busquem el botó "Close" ja que de poder editar aquest botó no hi seria
+        # Només surt com a únic botó si només tenim privilegis per veure
+        self.selenium.find_element(By.LINK_TEXT, "Close").click()
+
+        time.sleep(1)
